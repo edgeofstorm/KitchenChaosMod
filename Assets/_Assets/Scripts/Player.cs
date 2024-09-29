e84dac9eae14ac88b7ce7ffa06e1961f46f25525
edgeofstorm <enesekinci1907@gmail.com>
2024-09-29 16:33:35 +0300

    refactor - decoupled game inputs from player class

--- Assets/_Assets/Scripts/Player.cs
+++ Assets/_Assets/Scripts/Player.cs
@@ -11,54 +11,32 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float runMultiplier = 1f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking = false;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-            // isWalking = true;
-        }
-        // else
-        // {
-        //     isWalking = false;
-        // }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-
-        }
-
-
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
         isWalking = moveDir != new Vector3(0, 0, 0);
+
         moveDir = moveDir * Time.deltaTime * moveSpeed;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            moveDir.y = 3;
-        }
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
             moveDir *= runMultiplier;
 
         }
+
         transform.position += moveDir;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDir.y = 3;
+        }
+
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
