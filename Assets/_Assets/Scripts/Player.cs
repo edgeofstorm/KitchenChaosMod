360acd77c914d12f7ea2b9b78a947ff5dfa51787
edgeofstorm <enesekinci1907@gmail.com>
2024-09-30 12:20:03 +0300

    collision detect - capsulecast for player and player can still move holding two button combinations like "w-d"

--- Assets/_Assets/Scripts/Player.cs
+++ Assets/_Assets/Scripts/Player.cs
@@ -20,17 +20,46 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = 0.7f;
-        bool canMove = !Physics.Raycast(origin: transform.position, direction: moveDir, maxDistance: playerSize);
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        float moveDistance = Time.deltaTime * moveSpeed;
+        //point1 : top of the capsule
+        //point2 : bottom of the capsule
+        //radius : radius of the capsule
+        bool canMove = !Physics.CapsuleCast(point1: transform.position, point2: transform.position + Vector3.up * playerHeight, radius: playerRadius, direction: moveDir, maxDistance: moveDistance);
 
         isWalking = moveDir != new Vector3(0, 0, 0);
 
-        moveDir = moveDir * Time.deltaTime * moveSpeed;
+        moveDir = moveDir * moveDistance;
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
             moveDir *= runMultiplier;
         }
 
+
+        if (!canMove)
+        {
+            // attempt x axis movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(point1: transform.position, point2: transform.position + Vector3.up * playerHeight, radius: playerRadius, direction: moveDirX, maxDistance: moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // attempt z axis movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(point1: transform.position, point2: transform.position + Vector3.up * playerHeight, radius: playerRadius, direction: moveDirZ, maxDistance: moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+
+                }
+            }
+
+        }
         if (canMove)
         {
             transform.position += moveDir;
